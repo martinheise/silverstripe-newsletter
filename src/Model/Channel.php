8426b75c2887ffbf746ef7077049ca115ae2215f
Martin Heise <info@martinheise.de>
2025-10-02 18:05:07 +0200

    Setup backend editing of channels with custom gridfield for subscribers

--- src/Model/Channel.php
+++ src/Model/Channel.php
@@ -3,6 +3,15 @@
 namespace Mhe\Newsletter\Model;
 
 use Mhe\Newsletter\Controllers\NewsletterAdmin;
+use SilverStripe\Forms\DatetimeField;
+use SilverStripe\Forms\EmailField;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\GridField\GridField;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldExportButton;
+use SilverStripe\Forms\TextField;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\ManyManyList;
 use SilverStripe\Security\Permission;
@@ -23,7 +32,9 @@ class Channel extends DataObject
     ];
 
     private static array $summary_fields = [
-        'Title'
+        'Title',
+        'Subscribers.Count',
+        'ActiveSubscribers.Count',
     ];
 
     private static array $belongs_many_many = [
@@ -57,5 +68,47 @@ class Channel extends DataObject
     public function getActiveSubscribers(): ManyManyList
     {
         return $this->Subscribers()->filter('Confirmed:not', null);
+    }
+
+    public function getCMSFields(): FieldList
+    {
+        $fields = $this->scaffoldFormFields([
+            'includeRelations' => false,
+            'tabbed' => false,
+            'ajaxSafe' => true
+        ]);
+
+        if ($this->ID > 0) {
+            $gridConfig = GridFieldConfig_RelationEditor::create();
+            // enable export from here
+            // ToDo: Always / per default filter for confirmed subscribers?
+            $gridConfig->addComponent(new GridFieldExportButton("before", ['FullName', 'Email', 'Confirmed']));
+
+            // edit many_many_extraFields as detail form – ToDo: localization
+            $detailFields = new FieldList(
+                [
+                    new TextField('FullName'),
+                    new EmailField('Email'),
+                    new DatetimeField('ManyMany[Confirmed]')
+                ]
+            );
+            $detailForm = $gridConfig->getComponentByType(GridFieldDetailForm::class);
+            $detailForm->setFields($detailFields);
+
+            // modify grid columns – ToDo: localization
+            $data = $gridConfig->getComponentByType(GridFieldDataColumns::class);
+            $data->setDisplayFields(['FullName' => 'FullName', 'Email' => 'Email', 'Confirmed' => 'Confirmed']);
+
+            $fields->push(
+                GridField::create(
+                    'Subscribers',
+                    $this->fieldLabel('Subscribers'),
+                    $this->Subscribers(),
+                    $gridConfig
+                )
+            );
+        }
+        $this->extend('updateCMSFields', $fields);
+        return $fields;
     }
 }
